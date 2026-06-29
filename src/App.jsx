3c067eed84be2db2698acfeec6885f39a86c8ea1
Nithiesh <nithiesh164@gmail.com>
2026-06-29 20:18:34 +0530

    MVP V1.0 - Added Map rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,63 @@
 import { useState } from 'react'
 import './App.css'
+import Map from "./components/Map.jsx";
 
 function App() {
+    const [waypoints, setWaypoints] = useState([])
+    const [result, setResult] = useState(null)
 
-  const [waypoints, setWaypoints] = useState([])
-  const [result, setResult] = useState(null)
+    function addStops() {
+        setWaypoints([...waypoints, {latitude: "", longitude: ""}])
+    }
 
-  function addStops() {
-    setWaypoints([...waypoints, {latitude: "", longitude: ""}])
-  }
+    function handleInputChange(index, field, value) {
+        const newWaypoints = [...waypoints]
+        newWaypoints[index][field] = value
+        setWaypoints(newWaypoints)
+    }
 
-  function handleInputChange(index, field, value) {
-    const newWaypoints = [...waypoints]
-    newWaypoints[index][field] = value
-    setWaypoints(newWaypoints)
-  }
+    async function handleSubmit() {
+        const response = await fetch("http://localhost:8080/api/v1/route", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ routeWayPoints: waypoints})
+        })
+        const data = await response.json()
+        setResult(data)
+    }
 
-  async function handleSubmit() {
-    const response = await fetch("http://localhost:8080/api/v1/route", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({ routeWayPoints: waypoints})
-    })
-    const data = await response.json()
-    setResult(data)
-  }
+    return (
+        <>
+            {
+                waypoints.map((waypoint, index) => (
+                    <div key={index}>
+                        <input
+                            type="text"
+                            placeholder="latitude"
+                            onChange={(e) => handleInputChange(index, "latitude", e.target.value )}
+                        />
+                        <input type="text" placeholder="longitude" onChange={
+                            (e) => handleInputChange(index, "longitude", e.target.value )}/>
+                    </div>
+                ))
+            }
+            <button type={"button"} onClick={addStops}>add stop</button>
+            <button type={"submit"} onClick={handleSubmit}>submit</button>
 
-  return (
-      <>
-        {
-          waypoints.map((waypoint, index) => (
-              <div key={index}>
-                <input
-                    type="text"
-                    placeholder="latitude"
-                    onChange={(e) => handleInputChange(index, "latitude", e.target.value )}
-                />
-                <input type="text" placeholder="longitude" onChange={
-                  (e) => handleInputChange(index, "longitude", e.target.value )}/>
-              </div>
-          ))
-        }
-        <button type={"button"} onClick={addStops}>add stop</button>
-        <button type={"submit"} onClick={handleSubmit}>submit</button>
-
-        {result && (
-            <div>
-              <p>Total distance = {result.distance} meters</p>
-              <p>Total duration = {result.duration} seconds</p>
-              {
-                result.resultRouteWayPoints.map((coordinates, index) => (
-                  <div key={index} className="result">
-                    latitude: {coordinates.latitude}, longitude: {coordinates.longitude}
-                  </div>
-          ))}
-        </div>)}
-      </>
-)
-
+            {result && (
+                <div>
+                    <p>Total distance = {result.distance} meters</p>
+                    <p>Total duration = {result.duration} seconds</p>
+                    {
+                        result.resultRouteWayPoints.map((coordinates, index) => (
+                            <div key={index} className="result">
+                                latitude: {coordinates.latitude}, longitude: {coordinates.longitude}
+                            </div>
+                        ))}
+                </div>)}
+            <Map result={result} />
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Map.jsx
@@ -0,0 +1,36 @@
+import {MapContainer, Marker, Polyline, Popup, TileLayer} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+function Map(props) {
+
+    const result = props.result
+
+    return (
+        <>
+            <MapContainer
+                center={[52.517037, 13.388860]}
+                zoom={13}
+                scrollWheelZoom={false}
+                style={{height: '100vh', width: '100%'}}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {result && (
+                    result.resultRouteWayPoints.map((waypoint, index) => (
+                        <Marker key={index} position={[waypoint.latitude, waypoint.longitude]}  />
+                    ))
+                )}
+                {result && (
+                    <Polyline positions={[
+                        ...result.resultRouteWayPoints.map((wp) => [wp.latitude, wp.longitude]),
+                        [result.resultRouteWayPoints[0].latitude, result.resultRouteWayPoints[0].longitude]
+                    ]} />
+                )}
+            </MapContainer>
+        </>
+    )
+}
+
+export default Map
