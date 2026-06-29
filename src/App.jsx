40d147d41ae0eba381989f5405671774944fd64e
Nithiesh <nithiesh164@gmail.com>
2026-06-29 12:36:44 +0530

    MVP V1 - Basic app that inputs coordinates and returns the optimised coordinates back.

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [waypoints, setWaypoints] = useState([])
+  const [result, setResult] = useState(null)
+
+  function addStops() {
+    setWaypoints([...waypoints, {latitude: "", longitude: ""}])
+  }
+
+  function handleInputChange(index, field, value) {
+    const newWaypoints = [...waypoints]
+    newWaypoints[index][field] = value
+    setWaypoints(newWaypoints)
+  }
+
+  async function handleSubmit() {
+    const response = await fetch("http://localhost:8080/api/v1/route", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ routeWayPoints: waypoints})
+    })
+    const data = await response.json()
+    setResult(data)
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <>
+        {
+          waypoints.map((waypoint, index) => (
+              <div key={index}>
+                <input
+                    type="text"
+                    placeholder="latitude"
+                    onChange={(e) => handleInputChange(index, "latitude", e.target.value )}
+                />
+                <input type="text" placeholder="longitude" onChange={
+                  (e) => handleInputChange(index, "longitude", e.target.value )}/>
+              </div>
+          ))
+        }
+        <button type={"button"} onClick={addStops}>add stop</button>
+        <button type={"submit"} onClick={handleSubmit}>submit</button>
 
-      <div className="ticks"></div>
+        {result && (
+            <div>
+              <p>Total distance = {result.distance} meters</p>
+              <p>Total duration = {result.duration} seconds</p>
+              {
+                result.resultRouteWayPoints.map((coordinates, index) => (
+                  <div key={index} className="result">
+                    latitude: {coordinates.latitude}, longitude: {coordinates.longitude}
+                  </div>
+          ))}
+        </div>)}
+      </>
+)
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
 }
 
 export default App
